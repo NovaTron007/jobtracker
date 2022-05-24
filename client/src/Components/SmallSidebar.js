44e1c2d8a4c4ae0a9e3783efc92fbba6216e82f7
Dwayne Man <dwayne.man@gmail.com>
2022-05-24 16:02:49 +0800

    SmallSidebar: Created links file. Mapping links in SmallSidebar and using isActive function in NavLink to control active class.

--- client/src/Components/SmallSidebar.js
+++ client/src/Components/SmallSidebar.js
@@ -1,12 +1,49 @@
 import React from 'react'
+import { NavLink } from "react-router-dom"
+import { FaTimes } from "react-icons/fa"
+import Logo from "./Logo"
+import links from "../utils/links"
 import Wrapper from "../assets/Wrappers/SmallSidebar"
+import { useAppContext } from "../Context/AppContext" // get app global state
 
 const SmallSideBar = () => {
-  return (
-      <Wrapper>
-        <h4>SmallSideBar</h4>
-      </Wrapper>
-  )
+
+    // get global state, and functions
+    const { toggleSidebar, showSidebar } = useAppContext()
+
+
+    return (
+        <Wrapper>
+            <div className={showSidebar ? "sidebar-container" : "sidebar-container show-sidebar"}>
+                <div className="content">
+                    <button type="button" className="close-btn" onClick={toggleSidebar}>
+                        <FaTimes />
+                    </button>
+                    <header>
+                        <Logo />
+                    </header>
+                    <div className="nav-links">
+                        {links.map((item) => {
+                            // destructure current item object
+                            const { text, path, id, icon } = item
+                            // use return as two items inside map
+                            return (
+                                <NavLink
+                                    key={id}
+                                    to={path}
+                                    className={({ isActive }) => isActive ? "nav-link active" : "nav-link"} // navlink active class
+                                    onClick={toggleSidebar}
+                                >
+                                    <span className="icon">{icon}</span>
+                                    {text}
+                                </NavLink>
+                            )
+                        })}
+                    </div>
+                </div>
+            </div>
+        </Wrapper>
+    )
 }
 
 export default SmallSideBar
